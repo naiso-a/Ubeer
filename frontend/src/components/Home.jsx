259bcfc0737194bcbc46305b53fdedd7590a3fd0
naiso-a <martin.khyari@gmail.com>
2025-01-15 14:13:28 +0100

    Test jused apagnan

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -66,6 +66,7 @@ const Home = () => {
       <header className="home-header">
         <h1>Bienvenue sur Ubeer - Livraison de bière à domicile</h1>
         <h1>Je suis un connard</h1>
+        <h1>je ne suis pas un connard</h1>
         <p>Découvrez notre catalogue de bières, regroupées par brasserie, et ajoutez vos bières préférées au panier !</p>
       </header>
 
