c751dd9a925b61d8dcaf4544cf272ab156e78365
naiso-a <martin.khyari@gmail.com>
2025-01-16 11:02:43 +0100

    Auth0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Brewery from "./components/Brewery/Brewery";
 
 function App() {
   return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Brewery' element={<Brewery />} />
-        </Routes>
-      </Router>
-      
-    </div>
+    <Auth0Provider
+      domain="dev-r2ori0tgybwpisne.us.auth0.com" // Remplacez avec votre domaine Auth0
+      clientId="7r6A6dS64vv7dk0rcUuZzmAOoVKyFmqt" // Remplacez avec votre clientId Auth0
+      authorizationParams={{
+        redirect_uri: window.location.origin, // Redirige vers l'URL actuelle après connexion
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Brewery" element={<Brewery />} />
+          </Routes>
+        </Router>
+      </div>
+    </Auth0Provider>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../App.css';
-import "./Brewery/Brewery";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";  // Assurez-vous que le fichier LoginButton.jsx existe
+import LogoutButton from "./Logout";  // Assurez-vous que le fichier LogoutButton.jsx existe
 
 const Navbar = () => {
+  const { isAuthenticated } = useAuth0(); // Utilise le hook useAuth0 pour vérifier l'état de l'authentification
+
   return (
     <nav className="navbar">
       <h1 className="navbar-logo">Ubeer</h1>
       <ul className="navbar-links">
-        <li><a href="atalog">Catalogue</a></li>
+        <li><a href="/">Catalogue</a></li>
         <li><a href="./Brewery/Brewery">Brasseries</a></li>
         <li><a href="#cart">Panier</a></li>
+        <li>
+          {/* Si l'utilisateur est connecté, affiche le bouton de déconnexion, sinon le bouton de connexion */}
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        </li>
       </ul>
     </nav>
   );
